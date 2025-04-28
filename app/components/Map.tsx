bc470d7571e0f7dd49c6b018ce7372b149590136
saleem.jibril <saleemjibril5@gmail.com>
2025-04-28 21:59:11 +0100

    fixed window issue

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,6 +7,8 @@ const Map = ({ addresses }: { addresses: string[] }) => {
   const [map, setMap] = useState<google.maps.Map | null>(null);
   useEffect(() => {
     loader.importLibrary("maps").then(() => {
+          if (!!window) {
+      
       const geocoder = new window.google.maps.Geocoder();
 
       const mapOptions: google.maps.MapOptions = {
@@ -42,6 +44,7 @@ const Map = ({ addresses }: { addresses: string[] }) => {
           }
         });
       });
+    }
     });
   }, [addresses]);
   return <div id="map" style={{ height: "360px" }}></div>;
