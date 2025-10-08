c08eb7d0d3d960ffc8b45377d6745a9f0253b910
Saleem Jibril <saleemjibril5@gmail.com>
2025-10-08 13:52:27 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,14 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+export const metadata = {
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 };
-
 
 export default function RootLayout({
   children,
