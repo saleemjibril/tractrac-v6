3b2916b070349a21977b28de9c076a96afc8813f
Saleem Jibril <saleemjibril5@gmail.com>
2025-10-21 12:36:24 +0100

    update

--- app/components/contactInner.tsx
+++ app/components/contactInner.tsx
@@ -171,7 +171,7 @@ export default function ContactUsInner() {
                     <Icon viewBox="0 0 24 24" boxSize={5} color="#FA9411">
                       <path fill="currentColor" d="M6.62 10.79a15.91 15.91 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.01-.24c1.12.37 2.33.57 3.58.57a1 1 0 0 1 1 1v3.5a1 1 0 0 1-1 1C10.29 22 2 13.71 2 3.5a1 1 0 0 1 1-1H6.5a1 1 0 0 1 1 1c0 1.25.2 2.46.57 3.58a1 1 0 0 1-.25 1.01z" />
                     </Icon>
-                    <Text color="#333333" fontSize="16px">08064648720</Text>
+                    <Text color="#333333" fontSize="16px">07019898493</Text>
                   </HStack>
                 </Box>
               </Stack>
